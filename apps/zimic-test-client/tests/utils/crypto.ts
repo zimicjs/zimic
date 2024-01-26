edd7e73b3b3b9de6afa63bdc35bf42a7a1ab3e9d
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-01-26 20:55:25 -0300

    test(zimic-test-client): ignore crypto fallback from test coverage

--- apps/zimic-test-client/tests/utils/crypto.ts
+++ apps/zimic-test-client/tests/utils/crypto.ts
@@ -1,4 +1,6 @@
 export async function getCrypto() {
   const globalCrypto = globalThis.crypto as Crypto | undefined;
+  /* istanbul ignore next -- @preserve
+   * Ignoring as Node.js >=20 provides a global crypto and the import fallback won't run. */
   return globalCrypto ?? (await import('node:crypto'));
 }
