6bbc18c166e08b80410a333c3d5a7cc816f370ed
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-01-31 12:33:15 -0300

    docs(#zimic): add browser post install link to unregistered worker error

--- packages/zimic/src/interceptor/http/interceptorWorker/errors/UnregisteredServiceWorkerError.ts
+++ packages/zimic/src/interceptor/http/interceptorWorker/errors/UnregisteredServiceWorkerError.ts
@@ -5,7 +5,8 @@ class UnregisteredServiceWorkerError extends Error {
     super(
       `Failed to register the browser service worker: ` +
         `script '${window.location.origin}/${SERVICE_WORKER_FILE_NAME}' not found.\n\n` +
-        'Did you forget to run "npx zimic browser init <public-directory>"?',
+        'Did you forget to run "npx zimic browser init <public-directory>"?\n\n' +
+        'Learn more at https://github.com/diego-aquino/zimic#browser-post-install.',
     );
     this.name = 'UnregisteredServiceWorkerError';
   }
