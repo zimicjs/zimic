ff9935cd48c092eba17129624d55ca1a2800b505
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-02-13 16:42:07 -0300

    refactor(examples): improve names of example directories

--- release.config.js
+++ release.config.js
@@ -9,34 +9,6 @@ module.exports = {
       path: 'packages/zimic/package.json',
       versionKey: 'version',
     },
-    {
-      path: 'examples/with-node-vitest/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-node-jest/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-browser-vitest/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-browser-jest/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-bun/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-react/package.json',
-      versionKey: 'version',
-    },
-    {
-      path: 'examples/with-next-js/package.json',
-      versionKey: 'version',
-    },
   ],
   github: {
     repositoryOwner: 'diego-aquino',
