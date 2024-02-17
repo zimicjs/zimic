0a5a6efe9400069640d414a2b8fe795d4294a8a7
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-02-17 18:59:49 -0300

    refactor(examples): simplify jest configuration

--- examples/with-jest-jsdom/jest.config.js
+++ examples/with-jest-jsdom/jest.config.js
@@ -1,16 +1,11 @@
 module.exports = {
   roots: ['<rootDir>'],
-  clearMocks: true,
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
 
   testEnvironment: '<rootDir>/tests/environment.ts',
   resolver: `<rootDir>/tests/resolver.js`,
 
-  moduleDirectories: ['node_modules'],
-  moduleFileExtensions: ['js', 'ts', 'json'],
-
   testRegex: '\\.test\\.ts$',
-  testPathIgnorePatterns: ['<rootDir>/node_modules'],
 
   transform: {
     '^.+\\.(j|t)s$': ['@swc/jest'],
