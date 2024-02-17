a1b47abd80f4d82b729acc3b3c5cb9e92b89900c
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-02-17 19:48:20 -0300

    docs(examples-jest-jsdom): create example `README.md`

--- examples/with-jest-jsdom/tests/environment.ts
+++ examples/with-jest-jsdom/tests/environment.ts
@@ -1,10 +1,15 @@
 import JSDOMEnvironment from 'jest-environment-jsdom';
 
+/**
+ * We need this workaround because the global fetch API is not currently exposed by JSDOM.
+ *
+ * @see https://github.com/jsdom/jsdom/issues/1724
+ */
+
 class TestEnvironment extends JSDOMEnvironment {
   constructor(...parameters: ConstructorParameters<typeof JSDOMEnvironment>) {
     super(...parameters);
 
-    // https://github.com/jsdom/jsdom/issues/1724
     this.global.fetch = fetch;
     this.global.Headers = Headers;
     this.global.Request = Request;
--- examples/with-jest-jsdom/tests/resolver.js
+++ examples/with-jest-jsdom/tests/resolver.js
@@ -1,8 +1,13 @@
 /* eslint-disable */
 
-// https://github.com/mswjs/msw/issues/1786
+/**
+ * JSDOM runs on Node.js, but uses browser imports when present. This resolver removes the browser condition of
+ * MSW-related imports to prevent test runtime errors.
+ *
+ * @see https://github.com/mswjs/msw/issues/1786
+ */
+
 module.exports = (path, options) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   if (/^(msw|@mswjs\/interceptors)(\/|$)/.test(path)) {
     return options.defaultResolver(path, {
       ...options,
