6325379e8afe2f8be7a900a68ebdec13c5c8a9a1
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-02-23 15:11:45 -0300

    refactor(#zimic): rename request tracker tests to match class name

--- packages/zimic/src/interceptor/http/requestTracker/__tests__/InternalHttpRequestTracker.browser.test.ts
+++ packages/zimic/src/interceptor/http/requestTracker/__tests__/InternalHttpRequestTracker.browser.test.ts
@@ -2,7 +2,7 @@ import { describe } from 'vitest';
 
 import { declareSharedHttpRequestTrackerTests } from './shared/requestTrackerTests';
 
-describe('InternalHttpRequestTracker (browser)', () => {
+describe('HttpRequestTracker (browser)', () => {
   declareSharedHttpRequestTrackerTests({
     platform: 'browser',
   });
