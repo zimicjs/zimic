6a5bccc6ee4999033d1108bc06ee822dee1af075
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-03-01 21:13:42 -0300

    docs(#zimic): document headers, search params and json values

--- packages/zimic/src/http/headers/types.ts
+++ packages/zimic/src/http/headers/types.ts
@@ -1,9 +1,11 @@
 import { Defined } from '@/types/utils';
 
+/** A schema for strict HTTP headers. */
 export interface HttpHeadersSchema {
-  [paramName: string]: string | undefined;
+  [headerName: string]: string | undefined;
 }
 
+/** A strict tuple representation of a {@link HttpHeadersSchema}. */
 export type HttpHeadersSchemaTuple<Schema extends HttpHeadersSchema> = {
   [Key in keyof Schema & string]: [Key, Defined<Schema[Key]>];
 }[keyof Schema & string];
