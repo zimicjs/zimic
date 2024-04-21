d6ceacae270430303b16e4042c19f6ee268557bd
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-04-21 13:08:29 -0300

    feat(#zimic): create server start cli

--- packages/zimic/src/cli/cli.ts
+++ packages/zimic/src/cli/cli.ts
@@ -4,6 +4,7 @@ import { hideBin } from 'yargs/helpers';
 import { version } from '@@/package.json';
 
 import initializeBrowserServiceWorker from './browser/init';
+import startServer from './server/start';
 
 async function runCLI() {
   await yargs(hideBin(process.argv))
@@ -12,14 +13,49 @@ async function runCLI() {
       yargs
         .command(
           'init <publicDirectory>',
-          'Initialize the browser service worker',
+          'Initialize the browser service worker.',
           (yargs) =>
             yargs.positional('publicDirectory', {
               type: 'string',
-              description: 'The path to the public directory of your application',
+              description: 'The path to the public directory of your application.',
               demandOption: true,
             }),
           (cliArguments) => initializeBrowserServiceWorker(cliArguments),
+        )
+        .demandCommand(),
+    )
+    .command('server', 'Server', (yargs) =>
+      yargs
+        .command(
+          'start',
+          'Start the mock server.',
+          (yargs) =>
+            yargs
+              .option('hostname', {
+                type: 'string',
+                description: 'The hostname to start the server on.',
+                alias: 'h',
+                default: '0.0.0.0',
+              })
+              .option('port', {
+                type: 'number',
+                description: 'The port to start the server on.',
+                alias: 'p',
+              })
+              .option('on-ready', {
+                type: 'string',
+                description: 'A command to run when the server is ready.',
+              })
+              .option('on-ready-shell', {
+                type: 'string',
+                description: 'The shell to use when running the on-ready command. Defaults to the system shell.',
+              })
+              .option('ephemeral', {
+                type: 'boolean',
+                description: 'Whether the server should stop automatically after the on-ready command finishes.',
+                default: true,
+              }),
+          (cliArguments) => startServer(cliArguments),
         )
         .demandCommand(),
     )
---
+++ packages/zimic/src/cli/server/start.ts
@@ -0,0 +1,80 @@
+import { spawn } from 'child_process';
+
+import Server from './Server';
+
+async function runCommand(
+  command: string,
+  options: {
+    shell?: boolean | string;
+  },
+) {
+  await new Promise<void>((resolve, reject) => {
+    const onReadyProcess = spawn(command, {
+      shell: options.shell,
+      stdio: 'inherit',
+    });
+
+    onReadyProcess.once('exit', (code) => {
+      onReadyProcess.removeAllListeners();
+
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`The command '${command}' exited with code ${code}.`));
+      }
+    });
+
+    onReadyProcess.once('error', (error) => {
+      onReadyProcess.removeAllListeners();
+      reject(error);
+    });
+  });
+}
+
+const PROCESS_EXIT_EVENTS = [
+  'beforeExit',
+  'uncaughtExceptionMonitor',
+  'SIGINT',
+  'SIGTERM',
+  'SIGHUP',
+  'SIGBREAK',
+] as const;
+
+interface ServerStartOptions {
+  hostname: string;
+  port?: number;
+  onReady?: string;
+  onReadyShell?: string;
+  ephemeral: boolean;
+}
+
+async function startServer({
+  hostname,
+  port,
+  onReady: onReadyCommand,
+  onReadyShell: onReadyCommandShell,
+  ephemeral,
+}: ServerStartOptions) {
+  const server = new Server({ hostname, port });
+
+  for (const exitEvent of PROCESS_EXIT_EVENTS) {
+    process.on(exitEvent, async (exitCode: number) => {
+      await server.stop();
+      process.exit(exitCode);
+    });
+  }
+
+  await server.start();
+
+  if (onReadyCommand) {
+    await runCommand(onReadyCommand, {
+      shell: onReadyCommandShell ?? true,
+    });
+  }
+
+  if (ephemeral) {
+    await server.stop();
+  }
+}
+
+export default startServer;
