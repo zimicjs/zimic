46ad8538521d20f729c371c9760420e2097e7f31
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-06-01 10:49:49 -0300

    chore(examples-next-js-pages): ignore logs for requests to same host

--- examples/with-next-js-pages/tests/interceptors/utils.ts
+++ examples/with-next-js-pages/tests/interceptors/utils.ts
@@ -1,4 +1,17 @@
+import { http } from 'zimic/interceptor';
+
 import githubInterceptor, { githubFixtures } from './github';
+
+http.default.onUnhandledRequest(async (request, context) => {
+  const url = new URL(request.url);
+
+  // Ignore requests to the same host
+  if (url.host === window.location.host) {
+    return;
+  }
+
+  await context.log();
+});
 
 export async function loadInterceptors() {
   await githubInterceptor.start();
