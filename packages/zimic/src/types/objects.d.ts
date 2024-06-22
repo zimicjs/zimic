e92a7b0f234b5bda786353f05bd645c02d4a45d2
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-06-22 19:47:22 -0300

    refactor(#zimic): improve object constructor custom types

--- packages/zimic/src/types/objects.d.ts
+++ packages/zimic/src/types/objects.d.ts
@@ -1,7 +1,14 @@
+type ObjectKey<Type> = keyof Type & string;
+type ObjectValue<Type> = Type[ObjectKey<Type>];
+type ObjectEntry<Type> = [ObjectKey<Type>, ObjectValue<Type>];
+
 interface ObjectConstructor {
   // eslint-disable-next-line @typescript-eslint/method-signature-style
-  keys<Type>(object: Type): (keyof Type & string)[];
+  keys<Type>(object: Type): ObjectKey<Type>[];
 
   // eslint-disable-next-line @typescript-eslint/method-signature-style
-  entries<Type>(object: Type): [keyof Type & string, Type[keyof Type]][];
+  values<Type>(object: Type): ObjectValue<Type>[];
+
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  entries<Type>(object: Type): ObjectEntry<Type>[];
 }
