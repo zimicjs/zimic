e8de13b0f8dc1988c7041397300cbffce2ba475a
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-06-30 20:22:51 -0300

    test(#zimic): abstract test utilities

--- packages/zimic/src/cli/__tests__/typegen/openapi.cli.node.test.ts
+++ packages/zimic/src/cli/__tests__/typegen/openapi.cli.node.test.ts
@@ -3,7 +3,7 @@ import glob from 'fast-glob';
 import filesystem from 'fs/promises';
 import path from 'path';
 import prettier, { Options } from 'prettier';
-import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeAll, beforeEach, describe, expect, it, MockInstance, vi } from 'vitest';
 
 import { version } from '@@/package.json';
 
@@ -115,6 +115,77 @@ describe('Type generation (OpenAPI)', () => {
     });
   });
 
+  function verifyFilterFileWarnings(fixtureCase: TypegenFixtureCase, consoleSpies: { warn: MockInstance }) {
+    expect(consoleSpies.warn).toHaveBeenCalledTimes(fixtureCase.shouldWriteToStdout ? 2 : 1);
+
+    const message = consoleSpies.warn.mock.calls[0].join(' ');
+    expect(message).toMatch(/.*\[zimic\].* /);
+    expect(message).toContain(
+      `Warning: Filter could not be parsed and was ignored: ${chalk.yellow('invalid filter line')}`,
+    );
+  }
+
+  function verifyResponsesWarnings(fixtureCase: TypegenFixtureCase, spies: { log: MockInstance; warn: MockInstance }) {
+    const expectedNonNumericStatusCodes = ['2xx', '4XX', 'default'];
+
+    const expectedNumberOfWarnings = fixtureCase.shouldWriteToStdout
+      ? expectedNonNumericStatusCodes.length + 1
+      : expectedNonNumericStatusCodes.length;
+
+    expect(spies.warn).toHaveBeenCalledTimes(expectedNumberOfWarnings);
+
+    const messages = spies.warn.mock.calls
+      .slice(0, expectedNonNumericStatusCodes.length)
+      .map((argument) => argument.join(' '))
+      .sort();
+
+    for (const [index, nonNumericStatusCode] of expectedNonNumericStatusCodes.entries()) {
+      const message = messages[index];
+      expect(message).toMatch(/.*\[zimic\].* /);
+      expect(message).toContain(
+        `Warning: Response has non-numeric status code: ${chalk.yellow(nonNumericStatusCode)}. ` +
+          'Consider replacing it with a number, such as 200, 404, and 500. Only numeric status codes can ' +
+          'be used in interceptors.',
+      );
+    }
+  }
+
+  function verifySuccessMessage(
+    fixtureCase: TypegenFixtureCase,
+    outputFilePath: string,
+    spies: { log: MockInstance; warn: MockInstance },
+  ) {
+    let successMessage: string | undefined;
+
+    if (fixtureCase.shouldWriteToStdout) {
+      expect(spies.log).not.toHaveBeenCalled();
+      successMessage = spies.warn.mock.calls.at(-1)?.join(' ');
+    } else {
+      expect(spies.log).toHaveBeenCalledTimes(1);
+      successMessage = spies.log.mock.calls.at(-1)?.join(' ');
+    }
+
+    expect(successMessage).toBeDefined();
+    expect(successMessage).toMatch(/.*\[zimic\].* /);
+    expect(successMessage).toContain(`Generated ${chalk.green(outputFilePath)}`);
+    expect(successMessage).toMatch(/.*(\d+ms).*$/);
+  }
+
+  async function getGeneratedOutputContent(
+    fixtureCase: TypegenFixtureCase,
+    outputFilePath: string,
+    processWriteSpy: MockInstance<Parameters<typeof process.stdout.write>>,
+  ) {
+    if (fixtureCase.shouldWriteToStdout) {
+      expect(processWriteSpy).toHaveBeenCalledTimes(1);
+      expect(processWriteSpy).toHaveBeenCalledWith(expect.any(String), 'utf-8', expect.any(Function));
+      return processWriteSpy.mock.calls[0][0].toString();
+    } else {
+      expect(processWriteSpy).toHaveBeenCalledTimes(0);
+      return filesystem.readFile(outputFilePath, 'utf-8');
+    }
+  }
+
   describe.each(fixtureCaseEntries)('Schema: %s', (fixtureName, fixtureCases: TypegenFixtureCase[]) => {
     describe.each(fixtureFileTypes)('Type: %s', (fileType) => {
       for (const fixtureCase of fixtureCases) {
@@ -146,12 +217,12 @@ describe('Type generation (OpenAPI)', () => {
             ...fixtureCase.additionalArguments,
           ]);
 
+          let rawGeneratedOutputContent: string;
+
           const processWriteSpy = vi.spyOn(process.stdout, 'write').mockImplementation((_data, _encoding, callback) => {
             callback?.();
             return true;
           });
-
-          let rawOutputContent: string;
 
           try {
             await usingIgnoredConsole(['log', 'warn'], async (spies) => {
@@ -160,76 +231,29 @@ describe('Type generation (OpenAPI)', () => {
               const hasFilterFile = fixtureCase.additionalArguments.includes('--filter-file');
 
               if (hasFilterFile) {
-                expect(spies.warn).toHaveBeenCalledTimes(fixtureCase.shouldWriteToStdout ? 2 : 1);
-
-                const message = spies.warn.mock.calls[0].join(' ');
-                expect(message).toMatch(/.*\[zimic\].* /);
-                expect(message).toContain(
-                  `Warning: Filter could not be parsed and was ignored: ${chalk.yellow('invalid filter line')}`,
-                );
+                verifyFilterFileWarnings(fixtureCase, spies);
               } else if (fixtureName === 'responses') {
-                const expectedNonNumericStatusCodes = ['2xx', '4XX', 'default'];
-
-                const expectedNumberOfWarnings = fixtureCase.shouldWriteToStdout
-                  ? expectedNonNumericStatusCodes.length + 1
-                  : expectedNonNumericStatusCodes.length;
-
-                expect(spies.warn).toHaveBeenCalledTimes(expectedNumberOfWarnings);
-
-                const messages = spies.warn.mock.calls
-                  .slice(0, expectedNonNumericStatusCodes.length)
-                  .map((argument) => argument.join(' '))
-                  .sort();
-
-                for (const [index, nonNumericStatusCode] of expectedNonNumericStatusCodes.entries()) {
-                  const message = messages[index];
-                  expect(message).toMatch(/.*\[zimic\].* /);
-                  expect(message).toContain(
-                    `Warning: Response has non-numeric status code: ${chalk.yellow(nonNumericStatusCode)}. ` +
-                      'Consider replacing it with a number, such as 200, 404, and 500. Only numeric status codes can ' +
-                      'be used in interceptors.',
-                  );
-                }
+                verifyResponsesWarnings(fixtureCase, spies);
               } else {
                 expect(spies.warn).toHaveBeenCalledTimes(fixtureCase.shouldWriteToStdout ? 1 : 0);
               }
 
-              let successMessage: string | undefined;
-
-              if (fixtureCase.shouldWriteToStdout) {
-                expect(spies.log).not.toHaveBeenCalled();
-                successMessage = spies.warn.mock.calls.at(-1)?.join(' ');
-              } else {
-                expect(spies.log).toHaveBeenCalledTimes(1);
-                successMessage = spies.log.mock.calls[0].join(' ');
-              }
-
-              expect(successMessage).toBeDefined();
-              expect(successMessage).toMatch(/.*\[zimic\].* /);
-              expect(successMessage).toContain(`Generated ${chalk.green(outputFilePath)}`);
-              expect(successMessage).toMatch(/.*(\d+ms).*$/);
+              verifySuccessMessage(fixtureCase, outputFilePath, spies);
             });
 
-            if (fixtureCase.shouldWriteToStdout) {
-              expect(processWriteSpy).toHaveBeenCalledTimes(1);
-              expect(processWriteSpy).toHaveBeenCalledWith(expect.any(String), 'utf-8', expect.any(Function));
-              rawOutputContent = processWriteSpy.mock.calls[0][0].toString();
-            } else {
-              expect(processWriteSpy).toHaveBeenCalledTimes(0);
-              rawOutputContent = await filesystem.readFile(outputFilePath, 'utf-8');
-            }
+            rawGeneratedOutputContent = await getGeneratedOutputContent(fixtureCase, outputFilePath, processWriteSpy);
           } finally {
             processWriteSpy.mockRestore();
           }
 
-          const outputContent = normalizeGeneratedFileToCompare(
-            await prettier.format(rawOutputContent, { ...prettierConfig, parser: 'typescript' }),
+          const generatedOutputContent = normalizeGeneratedFileToCompare(
+            await prettier.format(rawGeneratedOutputContent, { ...prettierConfig, parser: 'typescript' }),
           );
           const expectedOutputContent = normalizeGeneratedFileToCompare(
             await filesystem.readFile(expectedOutputFilePath, 'utf-8'),
           );
 
-          expect(outputContent).toBe(expectedOutputContent);
+          expect(generatedOutputContent).toBe(expectedOutputContent);
         });
       }
     });
