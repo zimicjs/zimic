95145ca7e7c5d1564196d3ae07c9c54d3e93dcf8
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-08 14:41:00 -0300

    build(#zimic): improve build config (#255)
    ### Build
    - [#zimic] Improved the build config in `tsup.config.ts`, enabling
    splitting and generating ESM CLI and postinstall scripts.
    
    ### Refactoring
    - [#zimic] Improved the postinstall logic to patch MSW exports.

--- packages/zimic/tsup.config.ts
+++ packages/zimic/tsup.config.ts
@@ -1,7 +1,18 @@
 import { Options, defineConfig } from 'tsup';
 
+export function pickKeys<Type, Key extends keyof Type>(object: Type, keys: Key[]): Pick<Type, Key> {
+  return keys.reduce(
+    (pickedObject, key) => {
+      pickedObject[key] = object[key];
+      return pickedObject;
+    },
+    {} as Pick<Type, Key>, // eslint-disable-line @typescript-eslint/prefer-reduce-type-parameter
+  );
+}
+
 const sharedConfig: Options = {
   bundle: true,
+  splitting: true,
   sourcemap: true,
   treeshake: true,
   minify: false,
@@ -25,28 +36,25 @@ const neutralConfig = (['cjs', 'esm'] as const).map<Options>((format) => ({
   external: ['util', 'buffer', 'crypto'],
 }));
 
-const nodeConfig = (['cjs', 'esm'] as const).map<Options>((format) => ({
-  ...sharedConfig,
-  name: `node-${format}`,
-  platform: 'node',
-  format: [format],
-  dts: format === 'cjs',
-  entry: {
+const nodeConfig = (['cjs', 'esm'] as const).map<Options>((format) => {
+  const entry = {
     server: 'src/interceptor/server/index.ts',
     typegen: 'src/typegen/index.ts',
-  },
-}));
-
-const cliConfig = (['cjs'] as const).map<Options>((format) => ({
-  ...sharedConfig,
-  name: 'cli',
-  platform: 'node',
-  format: [format],
-  dts: false,
-  entry: {
     cli: 'src/cli/index.ts',
     'scripts/postinstall': 'scripts/postinstall.ts',
-  },
-}));
+  };
 
-export default defineConfig([...neutralConfig, ...nodeConfig, ...cliConfig]);
+  const dtsEntry = pickKeys(entry, ['server', 'typegen']);
+
+  return {
+    ...sharedConfig,
+    name: `node-${format}`,
+    platform: 'node',
+    format: [format],
+    dts: format === 'cjs' ? { entry: dtsEntry } : false,
+    entry,
+    external: ['./index.mjs'],
+  };
+});
+
+export default defineConfig([...neutralConfig, ...nodeConfig]);
