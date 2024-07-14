21fce70268c26ae2434b6dfd0cd5b01a2cbd03eb
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-14 10:55:44 -0300

    chore(zimic-test-client): add temporary `console.log()`

--- apps/zimic-test-client/tests/v0/interceptor/thirdParty/shared/default.ts
+++ apps/zimic-test-client/tests/v0/interceptor/thirdParty/shared/default.ts
@@ -53,6 +53,8 @@ async function declareDefaultClientTests(options: ClientTestOptionsByWorkerType)
   const notificationBaseURL = notificationInterceptor.baseURL();
 
   beforeAll(async () => {
+    console.log();
+
     await Promise.all(
       interceptors.map(async (interceptor) => {
         await interceptor.start();
