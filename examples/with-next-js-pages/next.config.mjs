6ccaa293d82aff6ed295bcf9401e84cdb5cf0666
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-10-25 14:28:01 -0300

    revert(example-next-js-pages): include `_http_common` as external module
    This reverts commit bb1bd8b3c2fdc7393a5010c48721a151d41012b7.

--- examples/with-next-js-pages/next.config.mjs
+++ examples/with-next-js-pages/next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['page.tsx', 'route.ts'],
-
-  // _http_common is not included as a built-in module in Next.js as of 14.2.13.
-  // We need to include it manually. This also means that we can't use `--turbo` right now.
-  // When https://github.com/vercel/next.js/issues/70262 lands in Next.js 14, we'll be able to remove this.
-  // Related: https://github.com/mswjs/msw/issues/2291.
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.externals = [...(config.externals || []), '_http_common'];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
