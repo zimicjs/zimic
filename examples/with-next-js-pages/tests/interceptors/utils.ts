e4746be5c826698e34ca430a7d22414d82050178
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-10-28 19:49:52 -0300

    docs(examples): igore requests to the same host in with-next-js-pages

--- examples/with-next-js-pages/tests/interceptors/utils.ts
+++ examples/with-next-js-pages/tests/interceptors/utils.ts
@@ -5,9 +5,12 @@ import githubInterceptor, { githubFixtures } from './github';
 httpInterceptor.default.onUnhandledRequest(async (request, context) => {
   const url = new URL(request.url);
 
-  if (url.hostname !== '127.0.0.1') {
-    await context.log();
+  // Ignore requests to the same host
+  if (url.host === window.location.host) {
+    return;
   }
+
+  await context.log();
 });
 
 export async function loadInterceptors() {
