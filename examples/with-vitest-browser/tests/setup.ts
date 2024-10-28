b7a51c4f366374cccadc50bef293a0fd89ae0317
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-10-28 19:49:04 -0300

    docs(examples): remove unecessary handlers for browsers

--- examples/with-vitest-browser/tests/setup.ts
+++ examples/with-vitest-browser/tests/setup.ts
@@ -1,17 +1,8 @@
 import '@testing-library/jest-dom';
 
 import { beforeAll, afterEach, afterAll } from 'vitest';
-import { httpInterceptor } from 'zimic/interceptor/http';
 
 import githubInterceptor from './interceptors/github';
-
-httpInterceptor.default.onUnhandledRequest(async (request, context) => {
-  const url = new URL(request.url);
-
-  if (url.hostname !== '127.0.0.1') {
-    await context.log();
-  }
-});
 
 beforeAll(async () => {
   await githubInterceptor.start();
