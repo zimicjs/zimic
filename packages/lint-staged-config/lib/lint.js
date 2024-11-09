a83e913742605761a0628e8c1615eb3458a8d63b
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-11-09 10:23:17 -0300

    chore(#lint-staged): simplify pre-commit command

--- packages/lint-staged-config/lib/lint.js
+++ packages/lint-staged-config/lib/lint.js
@@ -1,5 +1,3 @@
-const ignoredFilenamesWithoutExtension = ['.eslintrc', '.lintstagedrc', 'release.config', 'jest.config'];
-
 module.exports = {
-  [`**/!(${ignoredFilenamesWithoutExtension.join('|')})*.(j|t)s(x|)`]: ['pnpm lint --max-warnings 0'],
+  [`**/*.ts(x|)`]: ['pnpm lint --max-warnings 0'],
 };
