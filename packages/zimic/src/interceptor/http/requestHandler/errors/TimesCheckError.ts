1253a4a3a4e2c7b7e94f8a63500a405f104dae79
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-12-16 19:48:58 -0300

    refactor(#zimic): improve `TimesCheckError` message composition

--- packages/zimic/src/interceptor/http/requestHandler/errors/TimesCheckError.ts
+++ packages/zimic/src/interceptor/http/requestHandler/errors/TimesCheckError.ts
@@ -6,25 +6,30 @@ class TimesCheckError extends TypeError {
     limits: { min: number; max: number };
     timesStack: string | undefined;
   }) {
-    const formattedLimits =
-      options.limits.min === options.limits.max
-        ? `exactly ${options.limits.min} requests`
-        : `at least ${options.limits.min}${
-            Number.isFinite(options.limits.max) ? ` and at most ${options.limits.max}` : ''
-          } requests`;
-
     const timesStackWithoutInternalCalls = options.timesStack?.replace(/([^\n]+\n\s*){4}at /, '');
 
-    const formattedTimesStack = timesStackWithoutInternalCalls
-      ? `The failed \`handler.times()\` was declared at: \n    ${timesStackWithoutInternalCalls}`
-      : '';
-
     const message = [
-      `Expected ${formattedLimits}, but got ${options.numberOfRequests}.\n\n`,
-      formattedTimesStack,
+      'Expected ',
+      options.limits.min === options.limits.max ? 'exactly ' : 'at least ',
+      options.limits.min,
+      options.limits.min === options.limits.max && (options.limits.min === 1 ? ' request' : ' requests'),
+
+      options.limits.min !== options.limits.max &&
+        Number.isFinite(options.limits.max) &&
+        ` and at most ${options.limits.max}`,
+      options.limits.min !== options.limits.max && (options.limits.max === 1 ? ' request' : ' requests'),
+
+      ', but got ',
+      options.numberOfRequests,
+
+      '.\n\n',
+
+      timesStackWithoutInternalCalls &&
+        `The failed \`handler.times()\` was declared at: \n    ${timesStackWithoutInternalCalls}`,
+
       '\n\nLearn more: https://github.com/zimicjs/zimic/wiki/api‐zimic‐interceptor‐http#http-handlertimes',
     ]
-      .filter(isNonEmpty)
+      .filter((part) => isNonEmpty(part) && part !== false)
       .join('');
 
     super(message);
