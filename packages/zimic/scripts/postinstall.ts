23c9832f9c0c2dd1102ae836e757985d0cc4f10d
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-01-25 19:15:07 -0300

    fix(#zimic): patch msw missing undefined check

--- packages/zimic/scripts/postinstall.ts
+++ packages/zimic/scripts/postinstall.ts
@@ -4,14 +4,15 @@ import path from 'path';
 
 import { Override } from '@/types/utils';
 
-type MSWPackage = typeof mswPackage;
-type MSWExports = MSWPackage['exports'];
+export type MSWPackage = typeof mswPackage;
+export type MSWExports = MSWPackage['exports'];
+
+export const MSW_ROOT_DIRECTORY = path.join(require.resolve('msw'), '..', '..', '..');
+export const MSW_PACKAGE_PATH = path.join(MSW_ROOT_DIRECTORY, 'package.json');
+export const MSW_BROWSER_DIRECTORY = path.join(MSW_ROOT_DIRECTORY, 'lib', 'browser');
 
 async function patchMSWExports() {
-  const mswRootDirectory = path.join(require.resolve('msw'), '..', '..', '..');
-  const mswPackagePath = path.join(mswRootDirectory, 'package.json');
-
-  const mswPackageContentAsString = await filesystem.readFile(mswPackagePath, 'utf-8');
+  const mswPackageContentAsString = await filesystem.readFile(MSW_PACKAGE_PATH, 'utf-8');
   const mswPackageContent = JSON.parse(mswPackageContentAsString) as MSWPackage;
 
   const browserExports = mswPackageContent.exports['./browser'] as Override<
@@ -28,11 +29,32 @@ async function patchMSWExports() {
   nodeExports.browser = browserExports.browser;
 
   const patchedMSWPackageContentAsString = JSON.stringify(mswPackageContent, null, 2);
-  await filesystem.writeFile(mswPackagePath, patchedMSWPackageContentAsString);
+  await filesystem.writeFile(MSW_PACKAGE_PATH, patchedMSWPackageContentAsString);
+}
+
+// This is a temporary workaround. Once https://github.com/mswjs/msw/issues/2146 is fixed, we'll be able to remove it.
+async function patchMSWBrowserEntry() {
+  for (const indexFileName of ['index.js', 'index.mjs']) {
+    const mswBrowserPath = path.join(MSW_BROWSER_DIRECTORY, indexFileName);
+    const mswBrowserContent = await filesystem.readFile(mswBrowserPath, 'utf-8');
+
+    const patchedMSWBrowserContent = mswBrowserContent.replace(
+      `if (responseJson.type?.includes("opaque")) {
+      return;
+    }`,
+
+      `if (!request || responseJson.type?.includes("opaque")) {
+      return;
+    }`,
+    );
+
+    await filesystem.writeFile(mswBrowserPath, patchedMSWBrowserContent);
+  }
 }
 
 async function postinstall() {
   await patchMSWExports();
+  await patchMSWBrowserEntry();
 }
 
 export const postinstallPromise = postinstall();
