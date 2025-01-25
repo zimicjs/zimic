2a6067327e18d1ecd192932b0a4eb2882c638bbd
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-01-25 19:25:56 -0300

    chore(#zimic): upgrade msw to v2.7.0 (#423)

--- packages/zimic/scripts/__tests__/postinstall.node.test.ts
+++ packages/zimic/scripts/__tests__/postinstall.node.test.ts
@@ -1,15 +1,31 @@
-import mswPackageJSON from 'msw/package.json';
+import filesystem from 'fs/promises';
+import path from 'path';
 import { describe, expect, it } from 'vitest';
 
-import { postinstallPromise } from '../postinstall';
+import { MSW_PACKAGE_PATH, MSW_BROWSER_DIRECTORY, MSWPackage, postinstallPromise } from '../postinstall';
 
 describe('Post-install script', () => {
-  it('should remove the default MSW export limitations after installation', async () => {
+  it('should patch msw/package.json exports', async () => {
     await postinstallPromise;
 
-    const { exports } = mswPackageJSON;
+    const mswPackageContentAsString = await filesystem.readFile(MSW_PACKAGE_PATH, 'utf-8');
+    const mswPackageContent = JSON.parse(mswPackageContentAsString) as MSWPackage;
+
+    const { exports } = mswPackageContent;
 
     expect(exports['./browser'].node).toEqual(exports['./node'].node);
     expect(exports['./node'].browser).toEqual(exports['./browser'].browser);
   });
+
+  it.each(['index.js', 'index.mjs'])(
+    'should patch a missing undefined check in msw/browser/%s',
+    async (indexFileName) => {
+      await postinstallPromise;
+
+      const mswBrowserPath = path.join(MSW_BROWSER_DIRECTORY, indexFileName);
+      const mswBrowserContent = await filesystem.readFile(mswBrowserPath, 'utf-8');
+
+      expect(mswBrowserContent).toContain('if (!request || responseJson.type?.includes("opaque")) {');
+    },
+  );
 });
