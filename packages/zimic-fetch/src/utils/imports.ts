e543481e019b0d691f70f07b2438d0bca0a178d0
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-02-20 22:56:20 -0300

    test(#zimic-fetch): extend test coverage

--- packages/zimic-fetch/src/utils/imports.ts
+++ packages/zimic-fetch/src/utils/imports.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore next -- @preserve
+ * Ignoring as Node.js >=20 provides globals that may cause this dynamic import to not run. */
 export function createCachedDynamicImport<ImportType>(
   importModuleDynamically: () => Promise<ImportType>,
 ): () => Promise<ImportType> {
--- packages/zimic-fetch/tests/utils/fetch.ts
+++ packages/zimic-fetch/tests/utils/fetch.ts
@@ -1,24 +1,11 @@
 import { expect } from 'vitest';
 
-interface ExpectFetchErrorOptions {
-  canBeGenericFailure?: boolean;
-  canBeAborted?: boolean;
-}
-
-function getFetchErrorMessageRegex(options: ExpectFetchErrorOptions) {
-  const { canBeGenericFailure = true, canBeAborted = false } = options;
-
-  const errorMessageOptions = [
-    canBeGenericFailure && 'fetch failed',
-    canBeGenericFailure && 'Failed to fetch',
-    canBeAborted && 'This operation was aborted',
-    canBeAborted && 'signal is aborted without reason',
-  ].filter((option) => option !== false);
-
+function getFetchErrorMessageRegex() {
+  const errorMessageOptions = ['fetch failed', 'Failed to fetch'];
   return new RegExp(`^${errorMessageOptions.join('|')}$`);
 }
 
-export async function expectFetchError(responsePromise: Promise<Response>, options: ExpectFetchErrorOptions = {}) {
-  const errorMessageExpression = getFetchErrorMessageRegex(options);
+export async function expectFetchError(responsePromise: Promise<Response>) {
+  const errorMessageExpression = getFetchErrorMessageRegex();
   await expect(responsePromise).rejects.toThrowError(errorMessageExpression);
 }
