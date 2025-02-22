5b6a08fac3d4f7b7e6f86434027f963be9847734
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-02-22 14:00:32 -0300

    feat(#zimic): warn about package renaming on import

---
+++ packages/zimic/src/interceptor/server/__tests__/index.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+
+import { usingIgnoredConsole } from '@tests/utils/console';
+
+describe('Root entry point', () => {
+  it('should log a warning when imported', async () => {
+    await usingIgnoredConsole(['warn'], async (spies) => {
+      await import('../index');
+
+      expect(spies.warn).toHaveBeenCalledTimes(1);
+      expect(spies.warn).toHaveBeenCalledWith(
+        [
+          'NOTE: The package "zimic" has been renamed to "@zimic/interceptor".',
+          'Please replace "zimic" with "@zimic/interceptor" in your package.json and update your imports:',
+          '',
+          '  - Before',
+          '  + After',
+          '',
+          '- import ... from "zimic/interceptor/server"',
+          '+ import ... from "@zimic/interceptor/server"',
+        ].join('\n'),
+      );
+    });
+  });
+});
---
+++ packages/zimic/src/types/utils.ts
@@ -0,0 +1,1 @@
+export type PossiblePromise<Type> = Type | PromiseLike<Type>;
