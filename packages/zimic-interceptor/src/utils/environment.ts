040e117e03cdecfc15189db90d890df4644b60c1
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-03-02 15:51:01 -0300

    refactor(interceptor): remove msw dependency from remote interceptors

--- packages/zimic-interceptor/src/utils/environment.ts
+++ packages/zimic-interceptor/src/utils/environment.ts
@@ -1,3 +1,7 @@
+export function isServerSide() {
+  return typeof process !== 'undefined' && typeof process.versions !== 'undefined';
+}
+
 export function isClientSide() {
-  return typeof window !== 'undefined';
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
 }
