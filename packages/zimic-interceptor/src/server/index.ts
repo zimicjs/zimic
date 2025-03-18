5a70a3460f9b2bba59863c3dc1d869c3df02eee8
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-03-17 21:01:37 -0300

    feat(interceptor): replace `interceptorServer.create` with `createInterceptorServer` (#654) (#656)
    Closes #654.

--- packages/zimic-interceptor/src/server/index.ts
+++ packages/zimic-interceptor/src/server/index.ts
@@ -1,5 +1,3 @@
-import InterceptorServerNamespace from './namespace/InterceptorServerNamespace';
-
 export { default as RunningInterceptorServerError } from './errors/RunningInterceptorServerError';
 export { default as NotRunningInterceptorServerError } from './errors/NotRunningInterceptorServerError';
 
@@ -8,12 +6,4 @@ export type { InterceptorServer } from './types/public';
 
 export { DEFAULT_ACCESS_CONTROL_HEADERS, DEFAULT_PREFLIGHT_STATUS_CODE } from './constants';
 
-/**
- * A namespace of interceptor server resources for handling HTTP requests.
- *
- * @see {@link https://github.com/zimicjs/zimic/wiki/cli‐zimic‐server#zimic-server `zimic-interceptor server` API reference}
- * @see {@link https://github.com/zimicjs/zimic/wiki/getting‐started#remote-http-interceptors Remote HTTP Interceptors} .
- */
-export const interceptorServer = Object.freeze(new InterceptorServerNamespace());
-
-export type { InterceptorServerNamespace };
+export { createInterceptorServer } from './factory';
