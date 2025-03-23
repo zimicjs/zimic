6352e1dae4f95a6c669403f542fb5c12e5db551a
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-03-23 18:41:30 -0300

    feat(fetch): `FetchResponseError#toObject` (#679)
    Closes #677.

--- docs/wiki/api‐zimic‐fetch.md
+++ docs/wiki/api‐zimic‐fetch.md
@@ -28,6 +28,9 @@
 - [`FetchRequest`](#fetchrequest-1)
 - [`FetchResponse`](#fetchresponse)
 - [`FetchResponseError`](#fetchresponseerror)
+  - [`FetchResponseError#toObject`](#fetchresponseerrortoobject)
+    - [`FetchResponseError#toObject` arguments](#fetchresponseerrortoobject-arguments)
+    - [`FetchResponseError#toObject` return](#fetchresponseerrortoobject-return)
 - [Guides](#guides)
   - [Using headers](#using-headers)
   - [Using search params (query)](#using-search-params-query)
@@ -721,6 +724,66 @@ if (!response.ok) {
 }
 ```
 
+### `FetchResponseError#toObject`
+
+The method `fetchResponseError.toObject()` returns a plain object representation of the error. It is useful for
+serialization, debugging, and logging purposes.
+
+```ts
+const response = await fetch(`/users/${userId}`, {
+  method: 'GET',
+});
+
+if (!response.ok) {
+  const plainError = response.error.toObject();
+  console.log(JSON.stringify(plainError));
+  // {"name":"FetchResponseError","message":"...","request":{...},"response":{...}}
+}
+```
+
+#### `FetchResponseError#toObject` arguments
+
+`fetchResponseError.toObject(options?)`
+
+| Argument  | Type                                         | Description                            |
+| --------- | -------------------------------------------- | -------------------------------------- |
+| `options` | `FetchResponseErrorObjectOptions` (optional) | The options for serializing the error. |
+
+`options` is an object supporting the following properties:
+
+| Option        | Type                                  | Description                                                                      |
+| ------------- | ------------------------------------- | -------------------------------------------------------------------------------- |
+| `includeBody` | `boolean` (optional, default `false`) | Whether to include the body of the request and response in the serialized error. |
+
+> [!NOTE]
+>
+> When using `options.includeBody: true`, you can only call `toObject()` if the bodies of the request and response have
+> not been consumed yet. In case you access their bodies before or after calling `toObject()`, use
+> [`request.clone()`](https://developer.mozilla.org/docs/Web/API/Request/clone) and
+> [`response.clone()`](https://developer.mozilla.org/en-US/docs/Web/API/Response/clone) to allow multiple reads.
+
+```ts
+const response = await fetch(`/users/${userId}`, {
+  method: 'GET',
+});
+
+// Clone the response before reading the body
+const body = await response.clone().json();
+console.log(body);
+
+if (!response.ok) {
+  // `toObject()` can be called as the response body was consumed from the clone
+  const plainError = await response.error.toObject({ includeBody: true });
+  console.log(plainError);
+}
+```
+
+#### `FetchResponseError#toObject` return
+
+A plain object representing this error. If `options.includeBody` is `true`, the body of the request and response will be
+included and the return of this method will be a `Promise`. Otherwise, the return will be the plain object itself
+without the body.
+
 ## Guides
 
 ### Using headers
@@ -1124,8 +1187,8 @@ const users = await request.json(); // User[]
 
 `@zimic/fetch` fully types the responses of your requests based on your HTTP schema. If the response has a failure
 status code (4XX or 5XX), the `response.ok` property is `false` and you can throw the `response.error` property, which
-is will be `FetchResponseError` to be handled upper in the call stack. If you want to handle the error in the same place
-as the request, you can check the `response.status` or the `response.ok` properties.
+is will be `FetchResponseError` to be handled upper in the call stack. If you want to handle the error as soon as the
+response is received, you can check the `response.status` or the `response.ok` properties.
 
 ```ts
 import { type HttpSchema } from '@zimic/http';
@@ -1177,4 +1240,35 @@ if (!response.ok) {
 }
 
 const user = await response.json(); // User
+```
+
+When logging fetch response errors (e.g. in a global handler), consider using
+[`fetchResponseError.toObject()`](#fetchresponseerrortoobject) to get a plain object representation serializable to
+JSON.
+
+```ts
+if (error instanceof FetchResponseError) {
+  const plainError = error.toObject();
+  console.error(plainError);
+}
+```
+
+You can also use `JSON.stringify` or a logging library to serialize the error.
+
+```ts
+if (error instanceof FetchResponseError) {
+  const plainError = error.toObject();
+  console.error(JSON.stringify(plainError)); // Log in a single line
+}
+```
+
+Request and response bodies are not included by default. If you want to include them, use
+[`fetchResponseError.toObject({ includeBody: true })`](#fetchresponseerrortoobject-arguments). Note that the result will
+be a `Promise` if `includeBody` is `true`.
+
+```ts
+if (error instanceof FetchResponseError) {
+  const plainError = await error.toObject({ includeBody: true });
+  console.error(JSON.stringify(plainError));
+}
 ```
--- packages/zimic-fetch/src/client/errors/FetchResponseError.ts
+++ packages/zimic-fetch/src/client/errors/FetchResponseError.ts
@@ -1,6 +1,28 @@
-import { HttpSchema, HttpSchemaMethod, HttpSchemaPath } from '@zimic/http';
+import { HttpHeaders, HttpHeadersSchema, HttpSchema, HttpSchemaMethod, HttpSchemaPath } from '@zimic/http';
 
-import { FetchRequest, FetchResponse } from '../types/requests';
+import { FetchRequest, FetchRequestObject, FetchResponse, FetchResponseObject } from '../types/requests';
+
+/**
+ * Options for converting a {@link FetchResponseError `FetchResponseError`} into a plain object.
+ *
+ * @see {@link https://github.com/zimicjs/zimic/wiki/api‐zimic‐fetch#fetchresponseerrortoobject `FetchResponseError#toObject` API reference}
+ */
+export interface FetchResponseErrorObjectOptions {
+  includeBody?: boolean;
+}
+
+/**
+ * A plain object representation of a {@link FetchResponseError `FetchResponseError`}, compatible with JSON. It is useful
+ * for serialization, debugging, and logging purposes.
+ *
+ * @see {@link https://github.com/zimicjs/zimic/wiki/api‐zimic‐fetch#fetchresponseerrortoobject `FetchResponseError#toObject` API reference}
+ */
+export interface FetchResponseErrorObject {
+  name: string;
+  message: string;
+  request: FetchRequestObject;
+  response: FetchResponseObject;
+}
 
 /**
  * An error representing a response with a failure status code (4XX or 5XX).
@@ -39,6 +61,10 @@ import { FetchRequest, FetchResponse } from '../types/requests';
  *     console.log(response.error); // FetchResponseError<Schema, 'GET', '/users'>
  *     console.log(response.error.request); // FetchRequest<Schema, 'GET', '/users'>
  *     console.log(response.error.response); // FetchResponse<Schema, 'GET', '/users'>
+ *
+ *     const plainError = response.error.toObject();
+ *     console.log(JSON.stringify(plainError));
+ *     // {"name":"FetchResponseError","message":"...","request":{...},"response":{...}}
  *   }
  *
  * @see {@link https://github.com/zimicjs/zimic/wiki/api‐zimic‐fetch#fetchresponseerror `FetchResponseError` API reference}
@@ -58,6 +84,114 @@ class FetchResponseError<
 
   get cause() {
     return this.response;
+  }
+
+  /**
+   * Converts this error into a plain object. This method is useful for serialization, debugging, and logging purposes.
+   *
+   * @example
+   *   const fetch = createFetch<Schema>({
+   *     baseURL: 'http://localhost:3000',
+   *   });
+   *
+   *   const response = await fetch(`/users/${userId}`, {
+   *     method: 'GET',
+   *   });
+   *
+   *   if (!response.ok) {
+   *     const plainError = response.error.toObject();
+   *     console.log(JSON.stringify(plainError));
+   *     // {"name":"FetchResponseError","message":"...","request":{...},"response":{...}}
+   *   }
+   *
+   * @param options.includeBody Whether to include the body of the request and response in the output. Defaults to
+   *   `false`.
+   * @returns A plain object representing this error. If `options.includeBody` is `true`, the body of the request and
+   *   response will be included and the return of this method will be a `Promise`. Otherwise, the return will be the
+   *   plain object itself without the body.
+   * @see {@link https://github.com/zimicjs/zimic/wiki/api‐zimic‐fetch#fetchresponseerrortoobject `FetchResponseError#toObject` API reference}
+   */
+  toObject(options: { includeBody: true }): Promise<FetchResponseErrorObject>;
+  toObject(options?: { includeBody?: false }): FetchResponseErrorObject;
+  toObject(options?: FetchResponseErrorObjectOptions): Promise<FetchResponseErrorObject> | FetchResponseErrorObject;
+  toObject(
+    options: FetchResponseErrorObjectOptions = {},
+  ): Promise<FetchResponseErrorObject> | FetchResponseErrorObject {
+    const { includeBody = false } = options;
+
+    const partialObject = {
+      name: this.name,
+      message: this.message,
+    } satisfies Partial<FetchResponseErrorObject>;
+
+    if (!includeBody) {
+      const request = this.convertRequestToObject({ includeBody: false });
+      const response = this.convertResponseToObject({ includeBody: false });
+      return { ...partialObject, request, response };
+    }
+
+    return Promise.all([
+      this.convertRequestToObject({ includeBody: true }),
+      this.convertResponseToObject({ includeBody: true }),
+    ]).then(([request, response]) => ({ ...partialObject, request, response }));
+  }
+
+  private convertRequestToObject(options: { includeBody: true }): Promise<FetchRequestObject>;
+  private convertRequestToObject(options: { includeBody: false }): FetchRequestObject;
+  private convertRequestToObject(options: { includeBody: boolean }): Promise<FetchRequestObject> | FetchRequestObject {
+    const partialObject = {
+      url: this.request.url,
+      path: this.request.path,
+      method: this.request.method,
+      headers: this.convertHeadersToObject(this.request.headers),
+      cache: this.request.cache,
+      destination: this.request.destination,
+      credentials: this.request.credentials,
+      integrity: this.request.integrity,
+      keepalive: this.request.keepalive,
+      mode: this.request.mode,
+      redirect: this.request.redirect,
+      referrer: this.request.referrer,
+      referrerPolicy: this.request.referrerPolicy,
+    } satisfies Partial<FetchRequestObject>;
+
+    if (!options.includeBody) {
+      return partialObject;
+    }
+
+    return this.request.text().then((bodyAsText: string) => ({
+      ...partialObject,
+      body: bodyAsText.length > 0 ? bodyAsText : null,
+    }));
+  }
+
+  private convertResponseToObject(options: { includeBody: true }): Promise<FetchResponseObject>;
+  private convertResponseToObject(options: { includeBody: false }): FetchResponseObject;
+  private convertResponseToObject(options: {
+    includeBody: boolean;
+  }): Promise<FetchResponseObject> | FetchResponseObject {
+    const partialObject = {
+      url: this.response.url,
+      type: this.response.type,
+      status: this.response.status,
+      statusText: this.response.statusText,
+      ok: this.response.ok,
+      headers: this.convertHeadersToObject(this.response.headers),
+      redirected: this.response.redirected,
+    } satisfies Partial<FetchResponseObject>;
+
+    if (!options.includeBody) {
+      return partialObject;
+    }
+
+    return this.response.text().then((bodyAsText: string) => ({
+      ...partialObject,
+      body: bodyAsText.length > 0 ? bodyAsText : null,
+    }));
+  }
+
+  private convertHeadersToObject(headers: Headers) {
+    return HttpHeaders.prototype.toObject.call(headers) as HttpHeadersSchema;
   }
 }
 
