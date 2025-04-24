94380fee9d311df02a503a9bd69217dc000f4e98
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-04-24 17:34:26 -0300

    fix(web): remove duplicate page title

--- apps/zimic-web/src/pages/index.tsx
+++ apps/zimic-web/src/pages/index.tsx
@@ -7,7 +7,7 @@ function HomePage() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+    <Layout description={siteConfig.tagline}>
       <main className={styles.main}>
         <p className={styles.workInProgressNote}>
           <img src="/img/logo.svg" alt="Zimic" />
