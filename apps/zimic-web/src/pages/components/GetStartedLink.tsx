9c536b1769b1915d21e860e07e27730c9d14e8c2
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-05-07 17:11:14 -0300

    feat(web): add prop `as` to button component

--- apps/zimic-web/src/pages/components/GetStartedLink.tsx
+++ apps/zimic-web/src/pages/components/GetStartedLink.tsx
@@ -1,30 +1,28 @@
-import Link from '@docusaurus/Link';
-
-import Button, { Props as ButtonProps } from '@/components/common/Button';
+import Button, { LinkProps as LinkButtonProps } from '@/components/common/Button';
 import ArrowIcon from '@/components/icons/ArrowIcon';
 import { cn } from '@/utils/styles';
 
-interface Props extends Omit<ButtonProps, 'rightIcon'> {
+interface Props extends Omit<LinkButtonProps, 'as' | 'rightIcon'> {
   arrow?: boolean;
 }
 
 function GetStartedLink({ arrow = false, className, ...rest }: Props) {
   return (
-    <Link href="/docs" className="no-underline">
-      <Button
-        className={cn(arrow && 'group', className)}
-        rightIcon={
-          arrow && (
-            <div className="relative h-full before:absolute before:left-0 before:top-1/2 before:h-[0.1rem] before:w-1.5 before:-translate-y-1/2 before:rounded-full before:bg-white before:transition-transform">
-              <ArrowIcon aria-hidden="true" className="origin-left transition-transform group-hover:translate-x-1" />
-            </div>
-          )
-        }
-        {...rest}
-      >
-        Get started
-      </Button>
-    </Link>
+    <Button
+      as="link"
+      href="/docs"
+      className={cn('no-underline', arrow && 'group', className)}
+      rightIcon={
+        arrow && (
+          <div className="relative h-full before:absolute before:left-0 before:top-1/2 before:h-[0.1rem] before:w-1.5 before:-translate-y-1/2 before:rounded-full before:bg-white before:transition-transform">
+            <ArrowIcon aria-hidden="true" className="origin-left transition-transform group-hover:translate-x-1" />
+          </div>
+        )
+      }
+      {...rest}
+    >
+      Get started
+    </Button>
   );
 }
 
