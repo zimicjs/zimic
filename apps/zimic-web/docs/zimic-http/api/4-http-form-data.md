2bea35bbf93c87153eb05cc61550398b1db1740d
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-06-01 14:16:33 -0300

    docs(web): create `@zimic/http` http form data api reference

--- apps/zimic-web/docs/zimic-http/api/4-http-form-data.md
+++ apps/zimic-web/docs/zimic-http/api/4-http-form-data.md
@@ -6,10 +6,191 @@ slug: /http/api/http-form-data
 
 # `HttpFormData`
 
-:::info
+An extended HTTP form data object with a strictly-typed schema. Fully compatible with the built-in
+[`FormData`](https://developer.mozilla.org/docs/Web/API/FormData) class.
 
-🚧 This section is a work in progress.
+```ts
+import { HttpFormData } from '@zimic/http';
 
-Please use the [current documentation](https://github.com/zimicjs/zimic/wiki) while [zimic.dev](/) is not yet complete.
+const formData = new HttpFormData<{
+  files: File[];
+  description?: string;
+}>();
 
-:::
+formData.append('file', new File(['content'], 'file.txt', { type: 'text/plain' }));
+formData.append('description', 'My file');
+
+const files = formData.getAll('file');
+console.log(files); // [File { name: 'file.txt', type: 'text/plain' }]
+
+const description = formData.get('description');
+console.log(description); // 'My file'
+```
+
+## `formData.set()`
+
+Sets a form data value. If the value already exists, it will be replaced.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to set.
+2. `value`: the value to set for the form data field.
+3. `fileName` (optional): the name of the file to set when the second parameter is `Blob` or `File`.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/set).
+
+## `formData.append()`
+
+Appends a value to a form data field, or adds the field if it does not exist.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to append to.
+2. `value`: the value to append for the form data field.
+3. `fileName` (optional): the name of the file to append when the second parameter is `Blob` or `File`.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/append).
+
+## `formData.get()`
+
+Retrieves the value of a given form data field. If the value of the key is an array in the schema, use
+[`formData.getAll()`](#formdatagetall) instead.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to retrieve.
+
+**Returns**: the value of the form data field, or `null` if it does not exist.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/get).
+
+## `formData.getAll()`
+
+Retrieves all values of a given form data field. If the value of the key is not an array in the schema, use
+[`formData.get()`](#formdataget) instead.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to retrieve.
+
+**Returns**: an array of values for the form data field, or an empty array if it does not exist.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/getAll).
+
+## `formData.has()`
+
+Checks if a form data field with the given name exists.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to check.
+2. `value` (optional): the value of the form data field to check.
+
+**Returns**: `true` if the form data field exists, `false` otherwise.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/has).
+
+## `formData.delete()`
+
+Deletes a form data field with the given name.
+
+**Parameters**:
+
+1. `name`: the name of the form data field to delete.
+2. `value` (optional): the value of the form data field to delete. If not provided, all fields with the given name will
+   be deleted.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/delete).
+
+## `formData.forEach()`
+
+Executes a function for each form data (`name`, `value`) pair.
+
+**Parameters**:
+
+1. `callback`: a function that will be called for each form data field. It receives the following arguments:
+
+- `value`: the value of the form data field.
+- `name`: the name of the form data field.
+- `formData`: the `HttpFormData` instance itself.
+
+2. `thisArg` (optional): a value to use as `this` when executing `callback`.
+
+## `formData.keys()`
+
+**Returns**: an iterator over all form data field names.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/keys).
+
+## `formData.values()`
+
+**Returns**: an iterator over all form data field values.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/values).
+
+## `formData.entries()`
+
+**Returns**: an iterator over all form data (`name`, `value`) pairs.
+
+**Related**:
+
+- [MDN Reference](https://developer.mozilla.org/docs/Web/API/FormData/entries).
+
+## `formData.equals()`
+
+Compares an `HttpFormData` instance with another to check if they are equal. Equality is defined as having the same keys
+and values, regardless of the order of the keys.
+
+**Parameters**:
+
+1. `otherFormData`: another `HttpFormData` instance to compare with.
+
+**Returns**: `true` if the two form data objects are equal, `false` otherwise.
+
+## `formData.contains()`
+
+Checks if an `HttpFormData` instance contains all keys and values of another `HttpFormData` instance.
+
+**Parameters**:
+
+1. `otherFormData`: another `HttpFormData` instance to check against.
+
+**Returns**: `true` if the current form data contains all keys and values of the other form data, `false` otherwise.
+
+## `formData.toObject()`
+
+Converts the `HttpFormData` instance to a plain object. This method is useful for serialization and debugging purposes.
+
+**Returns**: a plain object representation of the form data. If a key has multiple values, the object will contain an
+array of values for that key. If the key has only one value, the object will contain its value directly, without an
+array, regardless of how the value was initialized when creating the form data object.
+
+```ts
+const formData = new HttpFormData<{
+  title: string;
+  content: Blob[];
+}>();
+
+formData.set('title', 'My title');
+formData.set('content', new Blob(['content 1'], { type: 'text/plain' }));
+formData.set('content', new Blob(['content 2'], { type: 'text/plain' }));
+
+const object = formData.toObject();
+console.log(object); // { title: 'My title', content: [Blob, Blob] }
+```
--- packages/zimic-http/src/formData/HttpFormData.ts
+++ packages/zimic-http/src/formData/HttpFormData.ts
@@ -3,36 +3,11 @@ import { ArrayItemIfArray, ReplaceBy } from '@zimic/utils/types';
 
 import { HttpFormDataSchema, HttpFormDataSchemaName, HttpFormDataSerialized } from './types';
 
-/**
- * An extended HTTP form data object with a strictly-typed schema. Fully compatible with the built-in
- * {@link https://developer.mozilla.org/docs/Web/API/FormData `FormData`} class.
- *
- * **IMPORTANT**: the input of `HttpFormData` and all of its internal types must be declared inline or as a type aliases
- * (`type`). They cannot be interfaces.
- *
- * @example
- *   import { HttpFormData } from '@zimic/http';
- *
- *   const formData = new HttpFormData<{
- *     files: File[];
- *     description?: string;
- *   }>();
- *
- *   formData.append('file', new File(['content'], 'file.txt', { type: 'text/plain' }));
- *   formData.append('description', 'My file');
- *
- *   const files = formData.getAll('file');
- *   console.log(files); // [File { name: 'file.txt', type: 'text/plain' }]
- *
- *   const description = formData.get('description');
- *   console.log(description); // 'My file'
- *
- * @see {@link https://github.com/zimicjs/zimic/wiki/api‐zimic‐http#httpformdata `HttpFormData` API reference}
- */
+/** @see {@link https://zimic.dev/docs/http/api/http-form-data `HttpFormData` API reference} */
 class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSchema.Loose> extends FormData {
   readonly _schema!: HttpFormDataSerialized<LooseSchema>;
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/set MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataset `formData.set()` API reference} */
   set<Name extends HttpFormDataSchemaName<this['_schema']>>(
     name: Name,
     value: Exclude<ArrayItemIfArray<NonNullable<LooseSchema[Name]>>, Blob>,
@@ -54,7 +29,7 @@ class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSc
     }
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/append MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataappend `formData.append()` API reference} */
   append<Name extends HttpFormDataSchemaName<this['_schema']>>(
     name: Name,
     value: Exclude<ArrayItemIfArray<NonNullable<LooseSchema[Name]>>, Blob>,
@@ -76,70 +51,55 @@ class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSc
     }
   }
 
-  /**
-   * Get the value of the entry associated to a key name.
-   *
-   * If the key might have multiple values, use {@link HttpFormData#getAll} instead.
-   *
-   * @param name The name of the key to get the value of.
-   * @returns The value associated with the key name, or `null` if the key does not exist.
-   * @see {@link https://developer.mozilla.org/docs/Web/API/FormData/get MDN Reference}
-   */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataget `formData.get()` API reference} */
   get<Name extends HttpFormDataSchemaName.NonArray<this['_schema']>>(
     name: Name,
   ): ReplaceBy<ReplaceBy<ArrayItemIfArray<this['_schema'][Name]>, undefined, null>, Blob, File> {
     return super.get(name) as never;
   }
 
-  /**
-   * Get all the values of the entry associated with a key name.
-   *
-   * If the key has at most a single value, use {@link HttpFormData#get} instead.
-   *
-   * @param name The name of the key to get the values of.
-   * @returns An array of values associated with the key name, or an empty array if the key does not exist.
-   * @see {@link https://developer.mozilla.org/docs/Web/API/FormData/getAll MDN Reference}
-   */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatagetall `formData.getAll()` API reference} */
   getAll<Name extends HttpFormDataSchemaName.Array<this['_schema']>>(
     name: Name,
   ): ReplaceBy<ArrayItemIfArray<NonNullable<this['_schema'][Name]>>, Blob, File>[] {
     return super.getAll(name) as never;
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/has MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatahas `formData.has()` API reference} */
   has<Name extends HttpFormDataSchemaName<this['_schema']>>(name: Name): boolean {
     return super.has(name);
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/delete MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatadelete `formData.delete()` API reference} */
   delete<Name extends HttpFormDataSchemaName<this['_schema']>>(name: Name): void {
     super.delete(name);
   }
 
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataforEach `formData.forEach()` API reference} */
   forEach<This extends HttpFormData<this['_schema']>>(
     callback: <Key extends HttpFormDataSchemaName<this['_schema']>>(
       value: ReplaceBy<ArrayItemIfArray<NonNullable<this['_schema'][Key]>>, Blob, File>,
       key: Key,
-      parent: HttpFormData<this['_schema']>,
+      formData: HttpFormData<this['_schema']>,
     ) => void,
     thisArg?: This,
   ): void {
     super.forEach(callback as (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg);
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/keys MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatakeys `formData.keys()` API reference} */
   keys(): FormDataIterator<HttpFormDataSchemaName<this['_schema']>> {
     return super.keys() as never;
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/values MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatavalues `formData.values()` API reference} */
   values(): FormDataIterator<
     ReplaceBy<ArrayItemIfArray<NonNullable<this['_schema'][HttpFormDataSchemaName<this['_schema']>]>>, Blob, File>
   > {
     return super.values() as never;
   }
 
-  /** @see {@link https://developer.mozilla.org/docs/Web/API/FormData/entries MDN Reference} */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataentries `formData.entries()` API reference} */
   entries(): FormDataIterator<
     [
       HttpFormDataSchemaName<this['_schema']>,
@@ -158,14 +118,7 @@ class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSc
     return super[Symbol.iterator]() as never;
   }
 
-  /**
-   * Checks if the data is equal to the other data. Equality is defined as having the same keys and values, regardless
-   * of the order of keys.
-   *
-   * @param otherData The other data to compare.
-   * @returns A promise that resolves with `true` if the data is equal to the other data, or `false` otherwise.
-   *   Important: both form data might be read while comparing.
-   */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdataequals `formData.equals()` API reference} */
   async equals<OtherSchema extends LooseSchema>(otherData: HttpFormData<OtherSchema>): Promise<boolean> {
     if (!(await this.contains(otherData))) {
       return false;
@@ -181,14 +134,7 @@ class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSc
     return true;
   }
 
-  /**
-   * Checks if the data contains the other data. This method is less strict than {@link HttpFormData#equals} and only
-   * requires that all keys and values in the other data are present in this data.
-   *
-   * @param otherData The other data to compare.
-   * @returns A promise that resolves with `true` if this data contains the other data, or `false` otherwise. Important:
-   *   both form data might be read while comparing.
-   */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatacontains `formData.contains()` API reference} */
   async contains<OtherSchema extends LooseSchema>(otherData: HttpFormData<OtherSchema>): Promise<boolean> {
     for (const [otherKey, otherValue] of otherData.entries()) {
       const values = super.getAll.call(this, otherKey);
@@ -218,30 +164,7 @@ class HttpFormData<LooseSchema extends HttpFormDataSchema.Loose = HttpFormDataSc
     return true;
   }
 
-  /**
-   * Converts this form data into a plain object. This method is useful for serialization and debugging purposes.
-   *
-   * **NOTE**: If a key has multiple values, the object will contain an array of values for that key. If the key has
-   * only one value, the object will contain its value directly, without an array, regardless of how the value was
-   * initialized when creating the form data.
-   *
-   * @example
-   *   const formData = new HttpFormData<{
-   *     title: string;
-   *     descriptions: string[];
-   *     content: Blob;
-   *   }>();
-   *
-   *   formData.set('title', 'My title');
-   *   formData.append('descriptions', 'Description 1');
-   *   formData.append('descriptions', 'Description 2');
-   *   formData.set('content', new Blob(['content'], { type: 'text/plain' }));
-   *
-   *   const object = formData.toObject();
-   *   console.log(object); // { title: 'My title', descriptions: ['Description 1', 'Description 2'], content: Blob { type: 'text/plain' } }
-   *
-   * @returns A plain object representation of this form data.
-   */
+  /** @see {@link https://zimic.dev/docs/http/api/http-form-data#formdatatoobject `formData.toObject()` API reference} */
   toObject() {
     const object = {} as this['_schema'];
 
