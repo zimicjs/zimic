fb6b6dac45f0394d8134a8a65fde5235a00fa35b
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-06-28 11:25:17 -0300

    test(interceptor): fix type errors

--- packages/zimic-interceptor/src/http/interceptor/__tests__/shared/bodies/blob.ts
+++ packages/zimic-interceptor/src/http/interceptor/__tests__/shared/bodies/blob.ts
@@ -274,7 +274,7 @@ export async function declareBlobBodyHttpInterceptorTests(options: RuntimeShared
             return {
               status: 200,
               headers: { 'content-type': 'application/octet-stream' },
-              body: new Blob([responseBuffer]),
+              body: responseBuffer,
             };
           }),
           interceptor,
