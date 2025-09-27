4a1e715dc2b008b931e7c2be3ceb75358763b1ea
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-09-27 18:23:18 -0300

    refactor(http): rename `createWildcardRegexFromPath`

--- packages/zimic-http/src/typegen/openapi/transform/filters.ts
+++ packages/zimic-http/src/typegen/openapi/transform/filters.ts
@@ -1,5 +1,5 @@
 import isNonEmpty from '@zimic/utils/data/isNonEmpty';
-import createWildcardPathPattern from '@zimic/utils/url/createWildcardPathPattern';
+import createWildcardRegexFromPath from '@zimic/utils/url/createWildcardRegexFromPath';
 import filesystem from 'fs/promises';
 import path from 'path';
 import color from 'picocolors';
@@ -27,8 +27,8 @@ export function parseRawFilter(rawFilter: string): TypePathFilter | undefined {
   }
 
   return {
-    methodPattern: new RegExp(`(?:${filteredMethodsOrWildcard.toUpperCase().replace(/,/g, '|').replace(/\*/g, '.*')})`),
-    pathPattern: createWildcardPathPattern(filteredPath),
+    methodRegex: new RegExp(`(?:${filteredMethodsOrWildcard.toUpperCase().replace(/,/g, '|').replace(/\*/g, '.*')})`),
+    pathRegex: createWildcardRegexFromPath(filteredPath),
     isNegativeMatch: filterModifier === '!',
   };
 }
