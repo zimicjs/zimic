0bb80c5f5b8bb9f8325b4e9d6fbb1f6dbe8fdb92
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-10-22 21:44:48 -0300

    style(web): format files

--- apps/zimic-web/src/pages/components/GradientBackground.tsx
+++ apps/zimic-web/src/pages/components/GradientBackground.tsx
@@ -1,20 +1,20 @@
 function GradientBackground() {
   return (
-    <div className="-z-1 fixed inset-0 overflow-hidden">
+    <div className="fixed inset-0 -z-1 overflow-hidden">
       <div
-        className="bg-highlight-600/5 dark:bg-highlight-600/30 h-128 w-128 -z-1 absolute -right-64 -top-64 rounded-full"
+        className="bg-highlight-600/5 dark:bg-highlight-600/30 absolute -top-64 -right-64 -z-1 h-128 w-128 rounded-full"
         style={{ filter: 'blur(8rem)' }}
       />
       <div
-        className="bg-highlight-500/5 dark:bg-highlight-500/10 -z-1 h-128 w-128 absolute left-0 top-24 rounded-full"
+        className="bg-highlight-500/5 dark:bg-highlight-500/10 absolute top-24 left-0 -z-1 h-128 w-128 rounded-full"
         style={{ filter: 'blur(8rem)' }}
       />
       <div
-        className="bg-highlight-400/5 dark:bg-highlight-400/10 -z-1 absolute bottom-36 right-8 h-96 w-96 rounded-full"
+        className="bg-highlight-400/5 dark:bg-highlight-400/10 absolute right-8 bottom-36 -z-1 h-96 w-96 rounded-full"
         style={{ filter: 'blur(8rem)' }}
       />
       <div
-        className="bg-highlight-300/5 dark:bg-highlight-300/15 -z-1 h-128 w-128 absolute -bottom-64 left-32 rounded-full"
+        className="bg-highlight-300/5 dark:bg-highlight-300/15 absolute -bottom-64 left-32 -z-1 h-128 w-128 rounded-full"
         style={{ filter: 'blur(10rem)' }}
       />
     </div>
