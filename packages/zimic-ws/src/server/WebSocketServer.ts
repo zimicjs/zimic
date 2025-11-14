2914fd20d88467eeadeb28942d6cede397a8da70
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-11-13 22:15:17 -0300

    feat(ws): create websocket server

---
+++ packages/zimic-ws/src/server/WebSocketServer.ts
@@ -0,0 +1,36 @@
+import { Server as HttpServer } from 'http';
+import { Server as HttpsServer } from 'https';
+
+import { ServerSocket } from './ServerSocket';
+import { closeServerSocket, openServerSocket } from './utils/lifecycle';
+
+export interface WebSocketServerOptions {
+  server: HttpServer | HttpsServer;
+}
+
+class WebSocketServer {
+  private server: HttpServer | HttpsServer;
+  private socket: ServerSocket;
+
+  isRunning = false;
+
+  constructor(options: WebSocketServerOptions) {
+    this.server = options.server;
+    this.socket = new ServerSocket({ server: options.server });
+  }
+
+  async start(options: { timeout?: number } = {}) {
+    await openServerSocket(this.server, this.socket, options);
+    this.isRunning = true;
+  }
+
+  async stop(options: { timeout?: number } = {}) {
+    try {
+      await closeServerSocket(this.server, this.socket, options);
+    } finally {
+      this.isRunning = false;
+    }
+  }
+}
+
+export default WebSocketServer;
