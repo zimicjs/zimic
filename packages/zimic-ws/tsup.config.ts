c7dd929e625db2c7156b83a8de715e8308283ee5
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-11-13 21:25:47 -0300

    feat(ws): set up package

---
+++ packages/zimic-ws/tsup.config.ts
@@ -0,0 +1,24 @@
+import { Options, defineConfig } from 'tsup';
+
+const sharedConfig: Options = {
+  bundle: true,
+  splitting: true,
+  sourcemap: true,
+  treeshake: true,
+  minify: false,
+  clean: true,
+  keepNames: false,
+};
+
+const neutralConfig = (['cjs', 'esm'] as const).map<Options>((format) => ({
+  ...sharedConfig,
+  name: `neutral-${format}`,
+  platform: 'neutral',
+  format: [format],
+  dts: format === 'cjs' ? { resolve: true } : false,
+  entry: {
+    index: 'src/index.ts',
+  },
+}));
+
+export default defineConfig([...neutralConfig]);
