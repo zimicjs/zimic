21bbd798269eef06d85ff8250218313bee9640ad
Diego Aquino <diegocruzdeaquino@gmail.com>
2025-11-30 18:55:49 -0300

    refactor(ws): standardize error names

--- packages/zimic-ws/src/errors/WebSocketCloseTimeoutError.ts
+++ packages/zimic-ws/src/errors/WebSocketCloseTimeoutError.ts
@@ -3,6 +3,6 @@ import { WebSocketTimeoutError } from './WebSocketTimeoutError';
 export class WebSocketCloseTimeoutError extends WebSocketTimeoutError {
   constructor(reachedTimeout: number) {
     super(`Web socket close timed out after ${reachedTimeout}ms.`);
-    this.name = 'WebSocketCloseTimeout';
+    this.name = 'WebSocketCloseTimeoutError';
   }
 }
--- packages/zimic-ws/src/errors/WebSocketOpenTimeoutError.ts
+++ packages/zimic-ws/src/errors/WebSocketOpenTimeoutError.ts
@@ -3,6 +3,6 @@ import { WebSocketTimeoutError } from './WebSocketTimeoutError';
 export class WebSocketOpenTimeoutError extends WebSocketTimeoutError {
   constructor(reachedTimeout: number) {
     super(`Web socket open timed out after ${reachedTimeout}ms.`);
-    this.name = 'WebSocketOpenTimeout';
+    this.name = 'WebSocketOpenTimeoutError';
   }
 }
