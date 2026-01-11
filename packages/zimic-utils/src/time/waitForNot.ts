4f00d64ab7b964315dc6013602e3f5ebdbcec4dc
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-01-11 12:27:39 -0300

    fix(utils): avoid including vitest in non-test environments

--- packages/zimic-utils/src/time/waitForNot.ts
+++ packages/zimic-utils/src/time/waitForNot.ts
@@ -1,10 +1,14 @@
-import { expect } from 'vitest';
-
+import { createCachedDynamicImport } from '@/import';
 import { PossiblePromise } from '@/types';
 
 import waitFor, { WaitForOptions } from './waitFor';
 
+const importVitest = createCachedDynamicImport(() => import('vitest'));
+
 async function waitForNot(callback: () => PossiblePromise<void>, options?: WaitForOptions) {
+  // We need to dynamically import Vitest to avoid including it in non-test environments.
+  const { expect } = await importVitest();
+
   const waitForPromise = waitFor(callback, { timeout: 100, ...options });
   await expect(waitForPromise).rejects.toThrowError();
 }
