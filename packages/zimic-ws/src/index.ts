5a8ea6d734b068af7c341870801d21f85182d5f8
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-02-08 17:35:10 -0300

    feat(ws): add `WebSocketReadyState`

--- packages/zimic-ws/src/index.ts
+++ packages/zimic-ws/src/index.ts
@@ -1,6 +1,15 @@
+export type {
+  WebSocketSchema,
+  WebSocketEvent,
+  WebSocketEventType,
+  WebSocketEventMap,
+  WebSocketMessageData,
+} from './types/schema';
+
 export { WebSocketCloseTimeoutError } from './errors/WebSocketCloseTimeoutError';
 export { WebSocketMessageAbortError } from './errors/WebSocketMessageAbortError';
 export { WebSocketOpenTimeoutError } from './errors/WebSocketOpenTimeoutError';
 export { WebSocketTimeoutError } from './errors/WebSocketTimeoutError';
 
 export { default as WebSocketClient } from './client/WebSocketClient';
+export type { WebSocketReadyState } from './client/types';
