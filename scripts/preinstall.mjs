d3860511159bc8719ace9f852b511fb272ad15d1
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-03-14 17:29:33 -0300

    refactor(root): improve preinstall script implementation

--- scripts/preinstall.mjs
+++ scripts/preinstall.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 const CI = process.env.CI === 'true';
 
-export async function ensureLinkedLocalPackagesInLockFile() {
+async function ensureLocalPackagesInLockFile() {
   const lockFile = await fs.promises.readFile('pnpm-lock.yaml', 'utf-8');
 
   const newLockFile = lockFile
@@ -14,7 +14,9 @@ export async function ensureLinkedLocalPackagesInLockFile() {
     // Remove the remaining @zimic/* entries that should no longer be in use after the replacement above.
     .replaceAll(/\n  '@zimic\/[^@]+@[^']+':[\s\S]*?(\n{2,}|$)/g, '\n');
 
-  if (newLockFile === lockFile) {
+  const isLockFileUsingLocalPackages = newLockFile === lockFile;
+
+  if (isLockFileUsingLocalPackages) {
     return;
   }
 
@@ -34,7 +36,7 @@ export async function ensureLinkedLocalPackagesInLockFile() {
 }
 
 async function main() {
-  await ensureLinkedLocalPackagesInLockFile();
+  await ensureLocalPackagesInLockFile();
 }
 
 void main();
