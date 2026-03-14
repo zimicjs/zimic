d277d40ccdca854859d350ad420f86d70dabe63d
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-03-14 17:32:47 -0300

    fix(ci): correctly force registry package reference (#1249)
    - refactor(ci): move pnpm-lock.yaml check to preinstall script
    - fix(ci): correctly force registry package reference
    - chore(root): remove deprecated turbo option

--- scripts/preinstall.mjs
+++ scripts/preinstall.mjs
@@ -1,6 +1,8 @@
 import fs from 'fs';
 
-export async function ensureLinkedLocalPackagesInLockFile() {
+const CI = process.env.CI === 'true';
+
+async function ensureLocalPackagesInLockFile() {
   const lockFile = await fs.promises.readFile('pnpm-lock.yaml', 'utf-8');
 
   const newLockFile = lockFile
@@ -12,13 +14,29 @@ export async function ensureLinkedLocalPackagesInLockFile() {
     // Remove the remaining @zimic/* entries that should no longer be in use after the replacement above.
     .replaceAll(/\n  '@zimic\/[^@]+@[^']+':[\s\S]*?(\n{2,}|$)/g, '\n');
 
-  if (newLockFile !== lockFile) {
-    await fs.promises.writeFile('pnpm-lock.yaml', newLockFile, 'utf-8');
+  const isLockFileUsingLocalPackages = newLockFile === lockFile;
+
+  if (isLockFileUsingLocalPackages) {
+    return;
   }
+
+  await fs.promises.writeFile('pnpm-lock.yaml', newLockFile, 'utf-8');
+
+  console.error('pnpm-lock.yaml was using @zimic/* packages from the registry instead of local links.');
+
+  if (CI) {
+    console.error('Please run `pnpm install` locally and commit the changes to pnpm-lock.yaml.');
+  } else {
+    console.error(
+      'The lock file is now fixed. Please run `pnpm install` again and commit the changes to pnpm-lock.yaml.',
+    );
+  }
+
+  process.exit(1);
 }
 
 async function main() {
-  await ensureLinkedLocalPackagesInLockFile();
+  await ensureLocalPackagesInLockFile();
 }
 
 void main();
