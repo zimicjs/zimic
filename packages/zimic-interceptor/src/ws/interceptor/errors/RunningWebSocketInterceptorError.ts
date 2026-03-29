cf0b54bfc665592c89e7125f7b516e69c495c9bb
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-03-29 14:55:14 -0300

    docs(interceptor): add experimental notes about websocket interceptors

--- packages/zimic-interceptor/src/ws/interceptor/errors/RunningWebSocketInterceptorError.ts
+++ packages/zimic-interceptor/src/ws/interceptor/errors/RunningWebSocketInterceptorError.ts
@@ -1,3 +1,4 @@
+/** WebSocket interceptors are experimental. The API is subject to change without a major version bump. Use with caution. */
 class RunningWebSocketInterceptorError extends Error {
   constructor(additionalMessage: string) {
     super(`The interceptor is running. ${additionalMessage}`);
