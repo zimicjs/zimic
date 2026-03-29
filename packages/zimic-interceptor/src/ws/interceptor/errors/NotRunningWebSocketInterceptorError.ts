06bfedc58ad02c6c34ec53f81ea23d69b6ec19bc
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-03-29 14:55:14 -0300

    docs(interceptor): add experimental notes about websocket interceptors

--- packages/zimic-interceptor/src/ws/interceptor/errors/NotRunningWebSocketInterceptorError.ts
+++ packages/zimic-interceptor/src/ws/interceptor/errors/NotRunningWebSocketInterceptorError.ts
@@ -1,3 +1,4 @@
+/** WebSocket interceptors are experimental. The API is subject to change without a major version bump. Use with caution. */
 class NotRunningWebSocketInterceptorError extends Error {
   constructor() {
     super('Interceptor is not running. Did you forget to call `await interceptor.start()`?');
--- packages/zimic-interceptor/src/ws/interceptor/errors/UnknownWebSocketInterceptorPlatformError.ts
+++ packages/zimic-interceptor/src/ws/interceptor/errors/UnknownWebSocketInterceptorPlatformError.ts
@@ -1,3 +1,4 @@
+/** WebSocket interceptors are experimental. The API is subject to change without a major version bump. Use with caution. */
 class UnknownWebSocketInterceptorPlatformError extends Error {
   /* istanbul ignore next -- @preserve
    * Ignoring because checking unknown platforms is currently not possible in our Vitest setup. */
