effc4b5020b74721387f6869bd06efe40fc8f7a0
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-04-04 11:00:29 -0300

    refactor(ws): improve websocket client api and implementation

--- packages/zimic-ws/src/client/WebSocketClient.ts
+++ packages/zimic-ws/src/client/WebSocketClient.ts
@@ -1,30 +1,77 @@
-import { WebSocketEvent, WebSocketEventType, WebSocketMessageData, WebSocketSchema } from '@/types/schema';
+import { PossiblePromise } from '@zimic/utils/types';
 
-import { ClientSocket } from './ClientSocket';
+import { WebSocketMessageData, WebSocketSchema } from '@/types/schema';
+
 import { closeClientSocket, openClientSocket } from './utils/lifecycle';
 
-export type WebSocketClientEventListener<Schema extends WebSocketSchema, Type extends WebSocketEventType<Schema>> = (
-  this: WebSocketClient<Schema>,
-  event: WebSocketEvent<Schema, Type>,
-) => unknown;
+export namespace WebSocketClient {
+  export type ReadyState =
+    | typeof WebSocketClient.CONNECTING
+    | typeof WebSocketClient.OPEN
+    | typeof WebSocketClient.CLOSING
+    | typeof WebSocketClient.CLOSED;
 
-type WebSocketClientRawEventListener = (this: ClientSocket, event: Event) => unknown;
+  // The schema is not used in the event types, but it's included for consistency and future extensibility.
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export type OpenEvent<_Schema extends WebSocketSchema> = globalThis.Event;
 
-class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
+  export type MessageEvent<Schema extends WebSocketSchema> = globalThis.MessageEvent<WebSocketMessageData<Schema>>;
+
+  // The schema is not used in the event types, but it's included for consistency and future extensibility.
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export type CloseEvent<_Schema extends WebSocketSchema> = globalThis.CloseEvent;
+
+  // The schema is not used in the event types, but it's included for consistency and future extensibility.
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export type ErrorEvent<_Schema extends WebSocketSchema> = globalThis.Event;
+
+  interface Events<Schema extends WebSocketSchema = WebSocketSchema> {
+    open: OpenEvent<Schema>;
+    message: MessageEvent<Schema>;
+    close: CloseEvent<Schema>;
+    error: ErrorEvent<Schema>;
+  }
+
+  export type EventType = keyof Events<WebSocketSchema>;
+
+  export type Event<
+    Schema extends WebSocketSchema = WebSocketSchema,
+    Type extends EventType = EventType,
+  > = Events<Schema>[Type];
+
+  interface EventListenerParameters<Schema extends WebSocketSchema> {
+    open: [event: Event<Schema, 'open'>];
+    message: [event: Event<Schema, 'message'>];
+    close: [event: Event<Schema, 'close'>];
+    error: [event: Event<Schema, 'error'>];
+  }
+
+  export type EventListener<Schema extends WebSocketSchema, Type extends EventType> = (
+    this: WebSocketClient<Schema>,
+    ...parameters: EventListenerParameters<Schema>[Type]
+  ) => PossiblePromise<void>;
+}
+
+type WebSocketClientRawEventListener = (this: WebSocket, event: Event) => PossiblePromise<void>;
+
+export class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
   WebSocket,
   `${string}EventListener` | `on${string}`
 > {
-  private socket?: ClientSocket;
-  private _binaryType: BinaryType = 'blob';
+  private socket?: WebSocket;
 
-  private _onopen: WebSocketClientEventListener<Schema, 'open'> | null = null;
-  private _onmessage: WebSocketClientEventListener<Schema, 'message'> | null = null;
-  private _onclose: WebSocketClientEventListener<Schema, 'close'> | null = null;
-  private _onerror: WebSocketClientEventListener<Schema, 'error'> | null = null;
+  #url: string;
+  #protocols?: string | string[];
+  #binaryType: BinaryType = 'blob';
+
+  #onopen: WebSocketClient.EventListener<Schema, 'open'> | null = null;
+  #onmessage: WebSocketClient.EventListener<Schema, 'message'> | null = null;
+  #onclose: WebSocketClient.EventListener<Schema, 'close'> | null = null;
+  #onerror: WebSocketClient.EventListener<Schema, 'error'> | null = null;
 
   private listeners: {
-    [Type in WebSocketEventType<Schema>]: Map<
-      WebSocketClientEventListener<Schema, Type>,
+    [Type in WebSocketClient.EventType]: Map<
+      WebSocketClient.EventListener<Schema, Type>,
       WebSocketClientRawEventListener
     >;
   } = {
@@ -34,41 +81,49 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
     error: new Map(),
   };
 
-  constructor(
-    private _url: string,
-    private protocols?: string | string[],
-  ) {}
+  constructor(socket: WebSocket);
+  constructor(_url: string, protocols?: string | string[]);
+  constructor(urlOrSocket: WebSocket | string, protocols?: string | string[]) {
+    if (typeof urlOrSocket === 'string') {
+      this.#url = urlOrSocket;
+    } else {
+      this.#url = urlOrSocket.url;
+      this.socket = urlOrSocket;
+    }
 
-  static CONNECTING = ClientSocket.CONNECTING;
+    this.#protocols = protocols;
+  }
+
+  static CONNECTING = WebSocket.CONNECTING;
 
   get CONNECTING() {
     return WebSocketClient.CONNECTING;
   }
 
-  static OPEN = ClientSocket.OPEN;
+  static OPEN = WebSocket.OPEN;
 
   get OPEN() {
     return WebSocketClient.OPEN;
   }
 
-  static CLOSING = ClientSocket.CLOSING;
+  static CLOSING = WebSocket.CLOSING;
 
   get CLOSING() {
     return WebSocketClient.CLOSING;
   }
 
-  static CLOSED = ClientSocket.CLOSED;
+  static CLOSED = WebSocket.CLOSED;
 
   get CLOSED() {
     return WebSocketClient.CLOSED;
   }
 
   get binaryType() {
-    return this.socket?.binaryType ?? this._binaryType;
+    return this.socket?.binaryType ?? this.#binaryType;
   }
 
   set binaryType(value: 'blob' | 'arraybuffer') {
-    this._binaryType = value;
+    this.#binaryType = value;
 
     if (this.socket) {
       this.socket.binaryType = value;
@@ -76,7 +131,7 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
   }
 
   get url() {
-    return this.socket?.url ?? this._url;
+    return this.socket?.url ?? this.#url;
   }
 
   get protocol() {
@@ -87,8 +142,9 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
     return this.socket?.extensions ?? '';
   }
 
-  get readyState() {
-    return this.socket?.readyState ?? ClientSocket.CLOSED;
+  get readyState(): WebSocketClient.ReadyState {
+    const readyState = this.socket?.readyState ?? WebSocket.CLOSED;
+    return readyState as WebSocketClient.ReadyState;
   }
 
   get bufferedAmount() {
@@ -96,13 +152,32 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
   }
 
   async open(options: { timeout?: number } = {}) {
-    this.socket = new ClientSocket(this._url, this.protocols);
+    const socket = new WebSocket(this.#url, this.#protocols);
 
-    if (this.socket.binaryType !== this._binaryType) {
-      this.socket.binaryType = this._binaryType;
+    if (socket.binaryType !== this.binaryType) {
+      socket.binaryType = this.binaryType;
     }
 
-    await openClientSocket(this.socket, options);
+    this.applyListeners(socket);
+
+    await openClientSocket(socket, options);
+
+    this.socket = socket;
+  }
+
+  private applyListeners(socket: WebSocket) {
+    for (const type of ['open', 'message', 'close', 'error'] as const) {
+      const unitaryListener = this[`on${type}`] as WebSocketClient.EventListener<Schema, typeof type> | null;
+      const rawUnitaryListener = unitaryListener ? this.listeners[type].get(unitaryListener) : undefined;
+
+      if (rawUnitaryListener) {
+        socket[`on${type}`] = rawUnitaryListener;
+      }
+
+      for (const rawListener of this.listeners[type].values()) {
+        socket.addEventListener(type, rawListener);
+      }
+    }
   }
 
   async close(code?: number, reason?: string, options: { timeout?: number } = {}) {
@@ -113,19 +188,6 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
     try {
       await closeClientSocket(this.socket, { ...options, code, reason });
     } finally {
-      this.onopen = null;
-      this.onmessage = null;
-      this.onclose = null;
-      this.onerror = null;
-
-      for (const [type, listenerToRawListener] of Object.entries(this.listeners)) {
-        for (const rawListener of listenerToRawListener.values()) {
-          this.socket.removeEventListener(type, rawListener);
-        }
-
-        listenerToRawListener.clear();
-      }
-
       this.socket = undefined;
     }
   }
@@ -134,9 +196,9 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
     this.socket?.send(data);
   }
 
-  addEventListener<Type extends WebSocketEventType<Schema>>(
+  addEventListener<Type extends WebSocketClient.EventType>(
     type: Type,
-    listener: (this: WebSocketClient<Schema>, event: WebSocketEvent<Schema, Type>) => unknown,
+    listener: WebSocketClient.EventListener<Schema, Type>,
     options?: boolean | AddEventListenerOptions,
   ) {
     const rawListener = listener.bind(this) as WebSocketClientRawEventListener;
@@ -145,9 +207,9 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
     this.listeners[type].set(listener, rawListener);
   }
 
-  removeEventListener<Type extends WebSocketEventType<Schema>>(
+  removeEventListener<Type extends WebSocketClient.EventType>(
     type: Type,
-    listener: (this: WebSocketClient<Schema>, event: WebSocketEvent<Schema, Type>) => unknown,
+    listener: WebSocketClient.EventListener<Schema, Type>,
     options?: boolean | EventListenerOptions,
   ) {
     const rawListener = this.listeners[type].get(listener);
@@ -159,42 +221,42 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
   }
 
   get onopen() {
-    return this._onopen;
+    return this.#onopen;
   }
 
-  set onopen(listener: WebSocketClientEventListener<Schema, 'open'> | null) {
-    this.setUnitaryEventListener('open', listener);
+  set onopen(listener: WebSocketClient.EventListener<Schema, 'open'> | null) {
+    this.setEventListener('open', listener);
   }
 
   get onmessage() {
-    return this._onmessage;
+    return this.#onmessage;
   }
 
-  set onmessage(listener: WebSocketClientEventListener<Schema, 'message'> | null) {
-    this.setUnitaryEventListener('message', listener);
+  set onmessage(listener: WebSocketClient.EventListener<Schema, 'message'> | null) {
+    this.setEventListener('message', listener);
   }
 
   get onclose() {
-    return this._onclose;
+    return this.#onclose;
   }
 
-  set onclose(listener: WebSocketClientEventListener<Schema, 'close'> | null) {
-    this.setUnitaryEventListener('close', listener);
+  set onclose(listener: WebSocketClient.EventListener<Schema, 'close'> | null) {
+    this.setEventListener('close', listener);
   }
 
   get onerror() {
-    return this._onerror;
+    return this.#onerror;
   }
 
-  set onerror(listener: WebSocketClientEventListener<Schema, 'error'> | null) {
-    this.setUnitaryEventListener('error', listener);
+  set onerror(listener: WebSocketClient.EventListener<Schema, 'error'> | null) {
+    this.setEventListener('error', listener);
   }
 
-  private setUnitaryEventListener<Type extends WebSocketEventType<Schema>>(
-    type: Type,
-    listener: WebSocketClientEventListener<Schema, Type> | null,
-  ) {
-    type PrivateUnitaryListener = typeof listener;
+  private setEventListener<
+    Type extends WebSocketClient.EventType,
+    Listener extends WebSocketClient.EventListener<Schema, Type> | null,
+  >(type: Type, listener: Listener) {
+    const listenerProperty = `_on${type}` as keyof WebSocketClient<Schema>;
 
     if (listener) {
       const rawListener = listener.bind(this) as WebSocketClientRawEventListener;
@@ -204,9 +266,9 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
         this.socket[`on${type}`] = rawListener;
       }
 
-      (this[`_on${type}`] as PrivateUnitaryListener) = listener;
+      (this[listenerProperty] as Listener) = listener;
     } else {
-      const currentListener = this[`_on${type}`] as PrivateUnitaryListener;
+      const currentListener = this[listenerProperty] as Listener;
 
       if (currentListener) {
         this.listeners[type].delete(currentListener);
@@ -216,13 +278,11 @@ class WebSocketClient<Schema extends WebSocketSchema> implements Omit<
         this.socket[`on${type}`] = null;
       }
 
-      (this[`_on${type}`] as PrivateUnitaryListener) = null;
+      (this[listenerProperty] as Listener | null) = null;
     }
   }
 
-  dispatchEvent<Type extends WebSocketEventType<Schema>>(event: WebSocketEvent<Schema, Type>) {
+  dispatchEvent<Type extends WebSocketClient.EventType>(event: WebSocketClient.Event<Schema, Type>) {
     return this.socket?.dispatchEvent(event) ?? false;
   }
 }
-
-export default WebSocketClient;
