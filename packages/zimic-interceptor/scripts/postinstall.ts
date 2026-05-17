0861db219424c6448147aedcddc7395999740536
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-05-17 09:43:45 -0300

    docs(interceptor): document msw patch

--- packages/zimic-interceptor/scripts/postinstall.ts
+++ packages/zimic-interceptor/scripts/postinstall.ts
@@ -9,6 +9,12 @@ export type MSWExports = MSWPackage['exports'];
 export const MSW_ROOT_DIRECTORY = path.join(require.resolve('msw'), '..', '..', '..');
 export const MSW_PACKAGE_PATH = path.join(MSW_ROOT_DIRECTORY, 'package.json');
 
+// MSW prevents importing browser resources in node environments and vice versa by using export conditions (null exports).
+// This is a problem for @zimic/interceptor because we automatically select the necessary MSW worker/server based on the
+// user's environment. Depending on the build system and runtime, export conditions are evaluated before the code is even
+// executed, causing preventable build errors. Since @zimic/interceptor already handles the environment detection and
+// uses dynamic imports to load the correct MSW entry point, we need to disable MSW's null export conditions by patching
+// its package.json after installation.
 async function patchMSWExports() {
   const mswPackageContentAsString = await fs.promises.readFile(MSW_PACKAGE_PATH, 'utf-8');
   const mswPackageContent = JSON.parse(mswPackageContentAsString) as MSWPackage;
