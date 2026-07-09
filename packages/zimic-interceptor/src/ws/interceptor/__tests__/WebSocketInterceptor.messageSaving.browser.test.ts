718e8bacdb1c30715051d0e36714bbca74ce67a0
Diego Aquino <diegocruzdeaquino@gmail.com>
2026-07-09 20:34:08 -0300

    feat(interceptor): continue websocket interceptor

--- packages/zimic-interceptor/src/ws/interceptor/__tests__/WebSocketInterceptor.messageSaving.browser.test.ts
+++ packages/zimic-interceptor/src/ws/interceptor/__tests__/WebSocketInterceptor.messageSaving.browser.test.ts
@@ -3,7 +3,7 @@ import { beforeAll, describe } from 'vitest';
 import { getBrowserBaseURL } from '@tests/utils/interceptors';
 
 import testMatrix from './shared/matrix';
-import { declareMessageSavingWebSocketInterceptorTests } from './shared/requestSaving';
+import { declareMessageSavingWebSocketInterceptorTests } from './shared/messageSaving';
 
 describe.each(testMatrix)('WebSocketInterceptor (browser, $type) > Message saving', ({ type }) => {
   let baseURL: string;
